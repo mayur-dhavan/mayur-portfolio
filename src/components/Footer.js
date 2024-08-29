988c49357f3a71b2ea4ac970fa66c0dcaa27d4ed
Mayur Dhavan <118877752+mayur-dhavan@users.noreply.github.com>
2024-08-29 13:43:05 +0530

    Update Footer.js
    Added social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import linkedin from "../assets/img/nav-icon1.svg";
+import facebook from "../assets/img/nav-icon2.svg";
+import instagram from "../assets/img/nav-icon3.svg";
+import github from "../assets/img/github.svg";
+
 
 export const Footer = () => {
   return (
@@ -16,9 +18,10 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/mayur-dhavan-50902922b/"><img src={linkedin} alt="LinkedIn" /></a>
+              <a href="https://www.facebook.com/mayur.dhavan.127/"><img src={facebook} alt="Facebook" /></a>
+              <a href="https://www.instagram.com/mayur_dhavan_/"><img src={instagram} alt="Instagram" /></a>
+               <a href="https://github.com/mayur-dhavan"><img src={github} alt="GitHub" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
