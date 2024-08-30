246a9035b57c955c7cc16e356131293ce374729b
Mayur Dhavan <mayurdhavan2244@gmail.com>
2024-08-30 16:44:44 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-// import { About  } from "./components/About";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Education } from "./components/Education";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { AboutSection } from "./components/AboutSection";
+
 function App() {
   return (
     <div className="App">
