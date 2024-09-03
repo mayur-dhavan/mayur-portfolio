27beec4c07e9d0f571b18f97501fcb8560eaae2a
Mayur Dhavan <mayurdhavan2244@gmail.com>
2024-09-03 13:02:39 +0530

    Scroll to top button added for more accessibilty to users

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,36 @@ import linkedin from "../assets/img/nav-icon1.svg";
 import facebook from "../assets/img/nav-icon2.svg";
 import instagram from "../assets/img/nav-icon3.svg";
 import github from "../assets/img/github.svg";
+import topArrow from "../assets/img/top-arrow.svg";
 
+import { useEffect } from 'react';
 
 export const Footer = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const button = document.querySelector('.back-to-top');
+      if (window.scrollY > 300) {
+        button.style.display = 'block';
+      } else {
+        button.style.display = 'none';
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
   return (
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+          <Col className="footer-logo" size={12} sm={6}>
+           <img onClick={scrollToTop} src={logo} alt="Mayur Portfolio Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
@@ -23,9 +43,12 @@ export const Footer = () => {
               <a href="https://www.instagram.com/mayur_dhavan_/" target="blank"><img src={instagram} alt="Instagram" /></a>
                <a href="https://github.com/mayur-dhavan" target="blank"><img src={github} alt="GitHub" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Mayur Portfolio | Copyright 2024. All Rights Reserved</p>
           </Col>
         </Row>
+        <button className="back-to-top" onClick={scrollToTop}>
+        <img src={topArrow}  alt="Back to Top" />
+        </button>
       </Container>
     </footer>
   )
--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -2,7 +2,7 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { Newsletter } from "./Newsletter";
 
 export const MailchimpForm = () => {
-  const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+  const postUrl = `https://formspree.io/f/xjkbzknr`;
 
   return (
     <>
