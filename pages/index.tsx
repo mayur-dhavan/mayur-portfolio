d3b7fe49b8a02a0d2e1934ef81001caf42392677
mayur-dhavan <mayurdhavan2244@gmail.com>
2025-08-18 18:17:27 +0530

    test2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,23 @@
 import type { NextPage } from 'next'
 import dynamic from 'next/dynamic'
-
-// Server-side rendered components (main content - good for SEO)
-const NavBar = dynamic(() => import('../src/components/NavBar').then(mod => ({ default: mod.NavBar })))
-const Banner = dynamic(() => import('../src/components/Banner').then(mod => ({ default: mod.Banner })))
-const Skills = dynamic(() => import('../src/components/Skills').then(mod => ({ default: mod.Skills })))
-const Projects = dynamic(() => import('../src/components/Projects').then(mod => ({ default: mod.Projects })))
-const AboutSection = dynamic(() => import('../src/components/AboutSection').then(mod => ({ default: mod.AboutSection })))
-const Contact = dynamic(() => import('../src/components/Contact').then(mod => ({ default: mod.Contact })))
-const Footer = dynamic(() => import('../src/components/Footer').then(mod => ({ default: mod.Footer })))
+// Server-rendered components
+import { NavBar } from '../src/components/NavBar'
+import { Banner } from '../src/components/Banner'
+import { Skills } from '../src/components/Skills'
+import { Projects } from '../src/components/Projects'
+import { AboutSection } from '../src/components/AboutSection'
+import { Contact } from '../src/components/Contact'
+import { Footer } from '../src/components/Footer'
 
 // Client-side only (for WebGL/Canvas components that need browser APIs)
-// const FluidWebGLBackground = dynamic(() => import('../src/components/FluidWebGLBackground'), { ssr: false })
-// const GalaxyBackground = dynamic(() => import('../src/components/GalaxyBackground').then(mod => ({ default: mod.default })), { ssr: false })
+const FluidWebGLBackground = dynamic(() => import('../src/components/FluidWebGLBackground'), { ssr: false })
+const GalaxyBackground = dynamic(() => import('../src/components/GalaxyBackground').then(mod => ({ default: mod.default })), { ssr: false })
 
 const Home: NextPage = () => {
   return (
     <div className="App">
-      {/* <GalaxyBackground />
-      <FluidWebGLBackground /> */}
+      <GalaxyBackground />
+      <FluidWebGLBackground />
       <div className="app-content">
         <NavBar />
         <Banner />
